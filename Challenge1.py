699c4b9c997b588b3a1233a658b8c8c2f5f156f5
Fatima Jafri <fatima.jafri123@live.com>
2020-11-13 10:31:49 +0500

    Done new Challenge. Please Check

---
+++ Challenge1.py
@@ -0,0 +1,14 @@
+#Write a function named whichtakes a single parameter, which is a string.
+#Should return a list of all the indexes in the string that have capital letters.
+
+def capital_indexes():
+    str1=input("Enter String for capital and small letters")
+    list1=[]
+    list2 = []
+    list1[:0] = str1
+    for i in list1:
+        if i.isupper() == True:
+            list2.append(list1.index(i))
+    print(list2)
+
+capital_indexes()
